e6e18162bef2d33d31ffa8348e9332b0ae604f1d
Chris533 <eskim0@qq.com>
2020-02-11 23:38:44 +0800

    Git init

---
+++ README.md
@@ -0,0 +1,17 @@
+# BunAdmin
+
+## How to use
+
+Download the code [or clone the repo](https://github.com/bunred/bunadmin):
+
+```sh
+git clone https://github.com/bunred/bunadmin.git
+cd bunadmin
+```
+
+Install it and run:
+
+```sh
+npm install
+npm run dev
+```
---
+++ pages/about.tsx
@@ -0,0 +1,35 @@
+import React from "react"
+import Container from "@material-ui/core/Container"
+import Typography from "@material-ui/core/Typography"
+import Box from "@material-ui/core/Box"
+import MuiLink from "@material-ui/core/Link"
+import ProTip from "../src/ProTip"
+import Link from "../src/Link"
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <MuiLink color="inherit" href="http://www.bunadmin.com/">
+        BunAdmin
+      </MuiLink>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  )
+}
+
+export default function About() {
+  return (
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          About BunAdmin
+        </Typography>
+        <Link href="/">Go to the main page</Link>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
+  )
+}
