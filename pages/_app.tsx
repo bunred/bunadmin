f48a61eced98fb9f1fb449ba07967921ed9b5740
Chris533 <eskim0@qq.com>
2020-02-19 16:58:34 +0800

    Update _app to Function code style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,37 +1,35 @@
 import React from "react"
-import App from "next/app"
+import { AppProps } from "next/app"
 import Head from "next/head"
 import { ThemeProvider } from "@material-ui/core/styles"
 import CssBaseline from "@material-ui/core/CssBaseline"
-import theme from "../src/theme"
+import defaultTheme from "../src/themes/defaultTheme"
 
-export default class MyApp extends App {
-  componentDidMount() {
-    // Remove the server-side injected CSS.
+const App = ({ Component, pageProps }: AppProps) => {
+  React.useEffect(() => {
     const jssStyles = document.querySelector("#jss-server-side")
     if (jssStyles) {
-      jssStyles.parentElement!.removeChild(jssStyles)
+      // @ts-ignore
+      jssStyles.parentElement.removeChild(jssStyles)
     }
-  }
+  }, [])
 
-  render() {
-    const { Component, pageProps } = this.props
-
-    return (
-      <>
-        <Head>
-          <title>My page</title>
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width"
-          />
-        </Head>
-        <ThemeProvider theme={theme}>
-          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-          <CssBaseline />
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </>
-    )
-  }
+  return (
+    <>
+      <Head>
+        <title>My page</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={defaultTheme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
+
+export default App
