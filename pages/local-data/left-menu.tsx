1c39889cfc4b6a2aec2a97e180d2bd8e0fc7d812
Chris533 <eskim0@qq.com>
2020-02-24 23:21:16 +0800

    Add page (local-data/notice)

--- pages/local-data/left-menu.tsx
+++ pages/local-data/left-menu.tsx
@@ -1,30 +1,7 @@
-import React, { useState } from "react"
+import React from "react"
 
-import CommonTable from "../../src/components/CommonTable"
-import { leftMenuColumns } from "../../src/modules/local_data/left_menu/columns"
-import { editableController } from "../../src/modules/local_data/left_menu/controllers/editable_controller"
-import rxSubscribe from "../../src/utils/local_database/rxSubscribe"
+import LocalLeftMenuContainer from "../../src/modules/local_data/left_menu"
 
-export default function RxDbPage() {
-  const [data, setData] = useState([])
-
-  React.useEffect(() => {
-    ;(async () => {
-      await rxSubscribe({
-        collection: "left_menu",
-        sort: { rank: "desc" },
-        callback: data => setData(data)
-      })
-    })()
-  }, [])
-
-  return (
-    <CommonTable
-      title="Local Data"
-      columns={leftMenuColumns}
-      data={data}
-      editable={editableController()}
-      parentChildData={(row, rows) => rows.find(a => a.name === row.parent)}
-    />
-  )
+export default function LeftMenuPage() {
+  return <LocalLeftMenuContainer />
 }
---
+++ pages/local-data/notice.tsx
@@ -0,0 +1,6 @@
+import React from "react"
+import LocalNoticeContainer from "../../src/modules/local_data/notice"
+
+export default function LocalNoticePage() {
+  return <LocalNoticeContainer />
+}
