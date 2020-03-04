d9851828fede3969bd15feb1df9aa05e13a11b96
Chris533 <eskim0@qq.com>
2020-03-04 10:10:36 +0800

    Add component (CubeSpinner)

--- pages/[group]/[name].tsx
+++ pages/[group]/[name].tsx
@@ -4,23 +4,23 @@ import DefaultLayout from "../../src/layouts/DefaultLayout"
 import {useRouter} from "next/router"
 import CorePages from "../../src/components/CorePages"
 import SignInContainer from "../../src/components/User/SignIn"
-
-interface Interface {
-  group: string
-  name: string
-}
+import securityController from "../../src/modules/local_data/auth/controllers/securityController"
+import CubeSpinner from "../../src/components/CommonBgs/CubeSpinner"
+import {ParsedUrlQuery} from "querystring"
 
 const ModulePage = () => {
   const [ready, setReady] = useState(false)
   const router = useRouter()
-  const { group, name } = (router.query as unknown) as Interface
+  const { group, name } = router.query as ParsedUrlQuery
 
   useEffect(() => {
+    ;(async () => {
     if (!group || !name) return
-    setReady(true)
+    await securityController({ setReady, router })
+    })()
   }, [group])
 
-  if (!ready) return null
+  if (!ready) return <CubeSpinner />
 
   let render
 
