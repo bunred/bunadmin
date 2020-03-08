252447405a92db775aba615a43e7e240ecb7b48c
Chris533 <eskim0@qq.com>
2020-03-08 08:50:41 +0800

    Add util (storedToken)

--- src/services/user/verifyService.tsx
+++ src/services/user/verifyService.tsx
@@ -1,35 +1,14 @@
 import request from "@/utils/scripts/request"
-import rxDb from "@/utils/database/rxConnect"
-import { Collection as Auth } from "@/core/auth/collections"
-import { Collection as Setting } from "@/core/setting/collections"
-import { Primary } from "@/core/auth/schema"
+import storedToken from "@/utils/scripts/storedToken"
 
 async function verifyService(): Promise<any> {
-  const authStore = Auth.name
-  const setting = Setting.name
-  const db = await rxDb()
+  const token = await storedToken()
 
-  // query username from bunadmin_setting
-  const settingRes = await db[setting]
-    .findOne()
-    .where("name")
-    .eq(Primary)
-    .exec()
-  const username = (settingRes && settingRes.value) || ""
-
-  // query user from auth_store
-  const localUser = await db[authStore]
-    .findOne()
-    .where(Primary)
-    .eq(username)
-    .exec()
-  const userToken = localUser && localUser.token
-
-  // query online user with token
+  // query remote user with token
   return request("/auth/me", {
     method: "POST",
     headers: {
-      Authorization: `Bearer ${userToken}`
+      Authorization: `Bearer ${token}`
     }
   })
 }
