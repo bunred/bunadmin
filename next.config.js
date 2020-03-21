30346863f01d8f908e8a402b6676a0645a2dc340
Chris533 <eskim0@qq.com>
2020-03-21 17:40:56 +0800

    Update next.config (ignore .md file)

--- next.config.js
+++ next.config.js
@@ -13,8 +13,15 @@ module.exports = phase => {
     },
     // webpack
     webpack: (config) => {
+      // alias
       config.resolve.alias['@'] = path.resolve(__dirname, 'src')
       config.resolve.alias['@plugins'] = path.resolve(__dirname, 'plugins')
+      // rules
+      config.module.rules.push({
+        // ignore .md file
+        test: /\.md$/,
+        use: [{ loader: "ignore-loader" }]
+      })
       return config
     },
   }
