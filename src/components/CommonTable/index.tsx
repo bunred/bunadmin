e70d21625664405e1cc8d7a5c0fcd90087d87671
Chris533 <eskim0@qq.com>
2020-03-21 11:22:09 +0800

    Update component (CommonTable add localization)

--- src/components/CommonTable/index.tsx
+++ src/components/CommonTable/index.tsx
@@ -1,13 +1,13 @@
-import React, { RefObject, useEffect, useRef, useState } from "react"
+import React from "react"
 
 import Head from "next/head"
 import MaterialTable from "material-table"
 import { useTheme } from "@material-ui/core/styles"
 import tableIcons from "./models/tableIcons"
 import { CommonTableProps } from "./models/types"
-import { addTdController } from "./controllers/addTdController"
 import { CommonTableDefaultProps as DefaultProps } from "./models/defaultProps"
-import rxSubscribe from "@/utils/database/rxSubscribe"
+import { useTranslation } from "react-i18next"
+import localization from "@/components/CommonTable/localization"
 
 export function CommonTableHead({ title }: { title?: string }) {
   return (
@@ -26,75 +26,24 @@ export function CommonTableHead({ title }: { title?: string }) {
   )
 }
 
-export default function CommonTable({ ...props }: CommonTableProps<any>) {
-  const {
-    collection,
-    sort,
-    listTitle,
-    style,
-    columns,
-    editable,
-    options,
-    actions
-  } = props
+export default function CommonTable(props: CommonTableProps<any>) {
+  const { t } = useTranslation("table")
   const theme = useTheme()
-  const [listData, setListData] = useState([])
-  const [selectedRow, setSelectedRow] = useState({ tableData: { id: null } })
-  const tableRef = useRef() as RefObject<HTMLDivElement>
-
-  useEffect(() => {
-    ;(async () => {
-      await rxSubscribe({
-        collection: collection,
-        sort: sort,
-        callback: data => {
-          setListData(data)
-          // create <td /> when click add/create button
-          addTdController({ tableRef })
-        }
-      })
-    })()
-  }, [])
-
-  const onRowClick = (_evt: any, selectedRow: any) =>
-    setSelectedRow(selectedRow)
-
-  const rowStyle = (rowData: { tableData: { id: any } }) => ({
-    backgroundColor:
-      selectedRow && selectedRow.tableData.id === rowData.tableData.id
-        ? "rgba(51, 102, 255, 0.04)"
-        : "#FFF"
-  })
-
-  const mtbRef = React.createRef()
 
   return (
-    <div ref={tableRef}>
-      <CommonTableHead title={listTitle} />
-      <MaterialTable
-        {...props}
-        tableRef={mtbRef}
-        style={style || DefaultProps.style}
-        title={listTitle}
-        columns={columns}
-        data={listData}
-        // editable
-        editable={editable || {}}
-        // localization props
-        localization={DefaultProps.localization}
-        // icons
-        icons={tableIcons({ theme })}
-        // on row click
-        onRowClick={onRowClick}
-        // options
-        options={
-          options
-            ? { ...DefaultProps.options, rowStyle, ...options }
-            : { ...DefaultProps.options, rowStyle }
-        }
-        // actions
-        actions={actions || DefaultProps.actions}
-      />
-    </div>
+    <MaterialTable
+      // style
+      style={DefaultProps.style}
+      // localization props
+      localization={localization({ t })}
+      // icons
+      icons={tableIcons({ theme })}
+      // options
+      options={{ ...DefaultProps.options, selection: false }}
+      // actions
+      actions={[]}
+      // more props
+      {...props}
+    />
   )
 }
--- src/components/CommonTable/models/types.tsx
+++ src/components/CommonTable/models/types.tsx
@@ -1,8 +1,4 @@
 import { MaterialTableProps } from "material-table"
 
 export interface CommonTableProps<RowData extends object>
-  extends MaterialTableProps<RowData> {
-  collection: string // collection name
-  sort?: string
-  listTitle?: string
-}
+  extends MaterialTableProps<RowData> {}
