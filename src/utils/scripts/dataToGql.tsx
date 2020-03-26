b63927d2aebde0d585ef17778811becc8f630a4d
Chris533 <eskim0@qq.com>
2020-03-26 23:10:27 +0800

    Add util (filtersToWhere)

--- src/utils/scripts/dataToGql.tsx
+++ src/utils/scripts/dataToGql.tsx
@@ -1,3 +1,5 @@
+import { Filter } from "material-table"
+
 /**
  * Material Table Data to Gql string
  */
@@ -28,4 +30,59 @@ export default function dataToGql({ data, nulls }: Props) {
 
   const objects = fields.join(", ")
   return `{${objects}}`
+}
+
+// FiltersProps
+interface FiltersProps<RowData extends object> {
+  filters: Filter<RowData>[]
+  search?:
+    | any
+    | {
+        key: "name"
+        operator: "_like"
+        value: ""
+      } // search words
+  operators?: any | { name: "_like"; parent_id: "_eq" } // per key's operator
+}
+
+export function filtersToWhere({
+  filters,
+  search,
+  operators
+}: FiltersProps<any>) {
+  const fields: string[] = []
+
+  let searchStr = search
+    ? `${search.key}: {${search.operator}: ${search.value}},`
+    : ""
+
+  filters.map(item => {
+    let value = item.value
+    const valueType = typeof value
+
+    if (!value) return
+
+    if (valueType === "object") return
+
+    const key = String(item.column.field)
+
+    // filter contains search key
+    if (search && key === search.key) searchStr = ""
+
+    let operator = "_eq" // default
+    if (operators && operators[key]) {
+      operator = operators[key]
+      switch (operator) {
+        case "_like":
+          value = `"%${value}%"`
+          break
+      }
+    }
+
+    fields.push(`${key}: {${operator}: "${value}"}`)
+  })
+
+  const objects = fields.join(", ")
+
+  return `{${searchStr} ${objects}}`
 }
