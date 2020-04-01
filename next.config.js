1e47ded2469d13dd738e1a03300bc11199e51634
Chris533 <eskim0@qq.com>
2020-04-01 11:52:36 +0800

    Update next.config (add Generate pluginsData.json)

--- next.config.js
+++ next.config.js
@@ -1,9 +1,29 @@
 const envCtrl = require("./env-ctrl")
 const path = require('path')
+const FileHound = require('filehound')
+const fs = require("fs")
 
 module.exports = phase => {
 
   const env = envCtrl(phase)
+
+  // Generate pluginsData.json START
+  // find all plugin initData files
+  const pluginsPath = path.resolve('plugins')
+  const pluginsInitFiles = FileHound.create()
+    .paths(pluginsPath)
+    .match('initData.tsx')
+    .findSync()
+  let jsonStr = JSON.stringify(pluginsInitFiles)
+
+  // replace to {team}-{group} path
+  jsonStr = jsonStr.replace(/\\\\/g, "/")
+  jsonStr = jsonStr.replace(/".*?\/plugins\/(.*?)"/gm, `"$1"`)
+
+  const name = 'pluginsData.json'
+  const savePath = path.resolve(__dirname, 'plugins', name)
+  fs.writeFile(savePath, jsonStr, 'utf8', () => { console.log(`Write to ${name}`) })
+  // Gen pluginsData.json END
 
   return {
     env,
