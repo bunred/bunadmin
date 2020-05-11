65131f816986f3d2d3de448a7b75975b8bb737a7
Chris533 <eskim0@qq.com>
2020-05-11 20:40:44 +0800

    Move custom_types into next.env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,14 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+declare module "react-eva-icons" {
+  export type EvaSize = "small"|"medium"|"large"|"xlarge"
+
+
+  export interface EvaIconProps {
+    name: string;
+    size: EvaSize;
+    fill?: string;
+  }
+
+  export default function EvaIcon(props: EvaIconProps): React.ReactElement<EvaIconProps>
+}
