53ab44534b0c62d98e9acaea353237017ae91656
Chris533 <ganchengs@gmail.com>
2020-06-08 09:04:49 +0800

    Add noticeSlice

---
+++ src/slices/noticeSlice.ts
@@ -0,0 +1,33 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from '@/utils/store';
+import {SeverityType} from "@/core/notice/types"
+
+interface NoticeState {
+  title: string
+  severity?: SeverityType | null
+  content?: string
+  key?: string | number
+}
+
+const initialState: NoticeState = {
+  title: "init-notice",
+};
+
+export const noticeSlice = createSlice({
+  name: 'notice',
+  initialState,
+  reducers: {
+    setNotice: (state, action: PayloadAction<NoticeState>) => {
+      state.title = action.payload.title;
+      state.severity = action.payload.severity;
+      state.content = action.payload.content;
+      state.key = action.payload.key || new Date().getTime() + Math.random()
+    },
+  },
+});
+
+export const { setNotice } = noticeSlice.actions;
+
+export const selectNotice = (state: RootState) => state.notice;
+
+export default noticeSlice.reducer;
--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../slices/counterSlice';
+import noticeReducer from '../slices/noticeSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    notice: noticeReducer
   },
 });
 
