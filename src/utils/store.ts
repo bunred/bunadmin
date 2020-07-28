ebf59450cd7816ff8c20bf93621af6c22c249d19
Chris533 <ganchengs@gmail.com>
2020-07-28 19:57:07 +0800

    Update store (add interface)

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,12 +1,23 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../slices/counterSlice';
-import noticeReducer from '../slices/noticeSlice';
+import {
+  configureStore,
+  ThunkAction,
+  Action,
+  EnhancedStore,
+  AnyAction
+} from "@reduxjs/toolkit";
+import counterReducer from "../slices/counterSlice";
+import noticeReducer from "../slices/noticeSlice";
+import { ThunkMiddlewareFor } from "@reduxjs/toolkit/src/getDefaultMiddleware";
 
-export const store = configureStore({
+export const store: EnhancedStore<
+  any,
+  AnyAction,
+  [ThunkMiddlewareFor<any>]
+> = configureStore({
   reducer: {
     counter: counterReducer,
     notice: noticeReducer
-  },
+  }
 });
 
 export type RootState = ReturnType<typeof store.getState>;
