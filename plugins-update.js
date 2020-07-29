16a232f7fb35e939f42f13c713bdac67894b8166
Chris533 <ganchengs@gmail.com>
2020-07-29 20:36:53 +0800

    Update package (sync-plugins ^1.1.1)

--- plugins-update.js
+++ plugins-update.js
@@ -3,10 +3,8 @@ const path = require("path")
 const savePath = path.resolve(__dirname, "plugins-info.json")
 const pluginsPath = path.resolve(__dirname, "./plugins")
 
-const pluginsPrefixUrl =
-  "https://raw.githubusercontent.com/bunred/bunadmin-plugins/master/navigation"
 const pluginsInfoArr = require("./plugins-info.json") || []
 
 const { syncPlugins } = require("@bunred/sync-plugins")
 
-syncPlugins({ savePath, pluginsPath, pluginsPrefixUrl, pluginsInfoArr })
+syncPlugins({ savePath, pluginsPath, pluginsInfoArr })
