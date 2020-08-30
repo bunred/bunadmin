defecc302631e92833d8599439b8d3d45ee13b96
Chris533 <ganchengs@gmail.com>
2020-08-30 10:24:00 +0800

    Update ENV (add UPLOAD_URL)

--- packages/bunadmin/src/utils/config/index.tsx
+++ packages/bunadmin/src/utils/config/index.tsx
@@ -7,6 +7,7 @@ interface EnvTypes {
   AUTH_PLUGIN: string
   MAIN_URL?: string
   AUTH_URL?: string
+  UPLOAD_URL?: string
   SITE_URLS: string[]
   SITE_NAME: string
   ON_I18N: boolean
@@ -21,6 +22,10 @@ export const ENV: EnvTypes = {
   MAIN_URL: process.env.NEXT_PUBLIC_MAIN_URL,
   AUTH_URL:
     process.env.NEXT_PUBLIC_AUTH_URL || process.env.NEXT_PUBLIC_MAIN_URL,
+  UPLOAD_URL:
+    process.env.NEXT_PUBLIC_UPLOAD_URL ||
+    process.env.NEXT_PUBLIC_AUTH_URL ||
+    process.env.NEXT_PUBLIC_MAIN_URL,
   SITE_URLS: strToArr(process.env.NEXT_PUBLIC_SITE_URLS),
   SITE_NAME: process.env.NEXT_PUBLIC_SITE_NAME || "BunAdmin",
   ON_I18N: process.env.NEXT_PUBLIC_ON_I18N === "true" || false,
