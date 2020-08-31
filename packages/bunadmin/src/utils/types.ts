187b32f99cb2a882a3a075f43f59e95478d9772b
Chris533 <ganchengs@gmail.com>
2020-08-31 22:27:32 +0800

    Update utils (types improve IPluginData)

--- packages/bunadmin/src/utils/types.ts
+++ packages/bunadmin/src/utils/types.ts
@@ -2,28 +2,22 @@ import { MenuType, SchemaType } from "@/core"
 import { TFunction } from "i18next"
 import { RefObject } from "react"
 
-type Plugin = {
-  disable_menu?: boolean
+export interface IPluginData {
+  id: SchemaType["id"]
+  name: SchemaType["name"]
+  team: SchemaType["team"]
+  group: SchemaType["group"]
+  label?: SchemaType["label"]
+  role?: SchemaType["role"]
+  customized?: SchemaType["customized"]
+  columns?: SchemaType["columns"] // Column<any>[] (JsonStr)
+  ignore_schema?: SchemaType["ignore_schema"]
+  icon?: MenuType["icon"]
+  icon_type?: MenuType["icon_type"]
+  parent?: MenuType["parent"]
+  rank?: MenuType["rank"]
+  ignore_menu?: MenuType["ignore_menu"]
 }
-
-type PluginSchema = Pick<
-  SchemaType,
-  | "id"
-  | "team"
-  | "group"
-  | "label"
-  | "name"
-  | "created_at"
-  | "customized"
-  | "columns"
-  | "ignore_schema"
->
-type PluginMenu = Pick<
-  MenuType,
-  "slug" | "icon" | "icon_type" | "parent" | "rank" | "ignore_menu"
->
-
-export interface IPluginData extends Plugin, PluginSchema, PluginMenu {}
 
 export type PluginColumns = {
   t: TFunction
