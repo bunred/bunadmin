eef5e461573e831418345bcac77f53ea6388f4e2
Chris533 <ganchengs@gmail.com>
2020-09-04 17:05:18 +0800

    Update pages/_document (fix Html)

--- packages/bunadmin/pages/_document.tsx
+++ packages/bunadmin/pages/_document.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import Document, { Head, Main, NextScript } from "next/document"
+import Document, { Html, Head, Main, NextScript } from "next/document"
 import { ServerStyleSheets } from "@material-ui/core/styles"
 import defaultTheme from "@/utils/themes/defaultTheme"
 import { resetServerContext } from "react-beautiful-dnd"
@@ -8,7 +8,7 @@ import { ENV } from "@/utils/config"
 export default class MyDocument extends Document {
   render() {
     return (
-      <html lang={ENV.I18N_CODE}>
+      <Html lang={ENV.I18N_CODE}>
         <Head>
           {/* PWA primary color */}
           <meta
@@ -24,34 +24,12 @@ export default class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     )
   }
 }
 
 MyDocument.getInitialProps = async ctx => {
-  // Resolution order
-  //
-  // On the server:
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. document.getInitialProps
-  // 4. app.render
-  // 5. page.render
-  // 6. document.render
-  //
-  // On the server with error:
-  // 1. document.getInitialProps
-  // 2. app.render
-  // 3. page.render
-  // 4. document.render
-  //
-  // On the client
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. app.render
-  // 4. page.render
-
   // Render app and page and get the context of the page with collected side effects.
   const sheets = new ServerStyleSheets()
   const originalRenderPage = ctx.renderPage
