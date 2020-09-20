9fe2af7fe8ec346b9c4655bf491fa756725e44a7
Chris533 <ganchengs@gmail.com>
2020-09-20 23:07:07 +0800

    Update components (ignore the common prefix)

--- packages/bunadmin/src/components/index.ts
+++ packages/bunadmin/src/components/index.ts
@@ -2,25 +2,36 @@ export { default as AnimatedRandomBG } from "./CommonBgs/AnimatedRandomBG"
 export { default as CubeSpinner } from "./CommonBgs/CubeSpinner"
 export { default as ConfirmDialog } from "./CommonDialog/ConfirmDialog"
 export { default as UploadConfirmDialog } from "./CommonDialog/UploadCustomDialog"
-export { default as CommonDrawer } from "./CommonDrawer"
-export { default as CommonError } from "./CommonError"
+export { default as CommonDrawer, default as Drawer } from "./CommonDrawer"
+export { default as CommonError, default as Error } from "./CommonError"
 import { ErrorProps as ErrorPropsType } from "./CommonError/models/types"
 export type ErrorProps = ErrorPropsType
 export { default as errorMessages } from "./CommonError/models/errorMessages"
 
-export { default as CommonSchema } from "./CommonSchema"
+export { default as CommonSchema, default as Schema } from "./CommonSchema"
 export { default as columnsController } from "./CommonSchema/controllers/columnsController"
 export { default as dataController } from "./CommonSchema/controllers/dataController"
 export * from "./CommonSchema/controllers/editableController"
 
 export * from "./CommonSelector"
-export { default as CommonSnackbar } from "./CommonSnackbar"
+export {
+  default as CommonSnackbar,
+  default as Snackbar
+} from "./CommonSnackbar"
 export { default as SnackMessage } from "./CommonSnackbar/Message"
 
-export { default as CommonTable, CommonTableHead } from "./CommonTable"
+export {
+  default as CommonTable,
+  default as Table,
+  CommonTableHead,
+  CommonTableHead as TableHead
+} from "./CommonTable"
 export { default as TableSkeleton } from "./CommonTable/components/TableSkeleton"
 
-export { CommonTableDefaultProps } from "./CommonTable/models/defaultProps"
+export {
+  CommonTableDefaultProps,
+  CommonTableDefaultProps as TableDefaultProps
+} from "./CommonTable/models/defaultProps"
 export * from "./CommonTable/models/editable"
 export { default as tableIcons } from "./CommonTable/models/tableIcons"
 
