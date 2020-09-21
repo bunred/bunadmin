5fc46dd5d90f214bc5f27a8ac985282f1c956136
Chris533 <ganchengs@gmail.com>
2020-09-21 16:23:19 +0800

    Rename plugin-blog to plugin-example-blog

---
+++ plugins/bunadmin-plugin-example-blog/utils/initData.ts
@@ -0,0 +1,34 @@
+import { IPluginData } from "@bunred/bunadmin"
+const plugin = "strapi-example-blog"
+const commonProps = {
+  team: "example",
+  group: "blog",
+  customized: true,
+  icon_type: "eva"
+}
+const data: IPluginData[] = [
+  {
+    ...commonProps,
+    id: "example_blog",
+    name: "example_blog",
+    label: "Blog",
+    icon: "file-text-outline",
+    ignore_schema: true
+  },
+  {
+    ...commonProps,
+    id: "example_blog_post",
+    name: "post",
+    label: "Post",
+    parent: "example_blog"
+  },
+  {
+    ...commonProps,
+    id: "example_blog_category",
+    name: "category",
+    label: "Category",
+    parent: "example_blog"
+  }
+]
+
+export default { plugin, data }
