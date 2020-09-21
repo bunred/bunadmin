dea4823e213397b65fbc01961c09f625fac15d55
Chris533 <ganchengs@gmail.com>
2020-09-21 10:52:05 +0800

    Update auth-buncms (authResponseKey)

--- plugins/bunadmin-auth-buncms/index.ts
+++ plugins/bunadmin-auth-buncms/index.ts
@@ -5,6 +5,6 @@ import signIn from "./sign-in"
 
 export { initData, users, signIn }
 
-export const authResponseKey = "uuid"
+export const authResponseKey = "id"
 export const authRequestUrl = "/auth/me"
 export const authRequestMethod = "POST"
