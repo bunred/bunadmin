caa06b0e62745fe5677a147dd6ff69312f810d3d
Chris533 <ganchengs@gmail.com>
2020-09-22 12:59:58 +0800

    Update auth-strapi (export SignIn)

--- plugins/bunadmin-auth-strapi/index.ts
+++ plugins/bunadmin-auth-strapi/index.ts
@@ -2,9 +2,9 @@ import initData from "./utils/initData"
 export * from "./utils/types"
 
 import users from "./users"
-import signIn from "./sign-in"
+import SignIn from "./sign-in"
 
-export { initData, signIn, users }
+export { initData, SignIn, users }
 
 export const authResponseKey = "id"
 export const authRequestUrl = "/users/me"
