51523b093dc250bb80095746ec9d8739672d3b40
Chris533 <ganchengs@gmail.com>
2020-09-28 14:16:32 +0800

    Update utils (add withoutLayout)

--- packages/bunadmin/src/utils/scripts/index.ts
+++ packages/bunadmin/src/utils/scripts/index.ts
@@ -1,5 +1,27 @@
+import { ENV } from "@/utils"
+
 export { default as dataToGql } from "./dataToGql"
+export { default as handleSpecialPlugin } from "./handleSpecialPlugin"
 export { default as initData } from "./initData"
 export { fixTreeDataTr } from "./muiTable"
 export { default as request } from "./request"
 export { default as storedToken } from "./storedToken"
+
+/**
+ * handle PATHS_WITHOUT_LAYOUT
+ * @param group
+ * @param name
+ */
+export function withoutLayout(
+  group: string | string[],
+  name: string | string[]
+): boolean {
+  const path = `/${group}/${name}`
+  for (let i = 0; i < ENV.PATHS_WITHOUT_LAYOUT.length; i++) {
+    const item = ENV.PATHS_WITHOUT_LAYOUT[i]
+    const itemRegx = new RegExp(`${item}.*`, "g")
+    if (itemRegx.test(path)) return true
+  }
+
+  return false
+}
