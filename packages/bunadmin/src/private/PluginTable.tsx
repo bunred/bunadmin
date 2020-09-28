726e4dd243744a7d9ead325f48d32cafadb17ef9
Chris533 <ganchengs@gmail.com>
2020-09-28 11:43:03 +0800

    Add private/PluginTable

---
+++ packages/bunadmin/src/private/PluginTable.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+import dynamic from "next/dynamic"
+import TableSkeleton from "../components/CommonTable/components/TableSkeleton"
+import { PluginTableProps } from "../utils"
+import handleSpecialPlugin from "../utils/scripts/handleSpecialPlugin"
+
+/**
+ * !DO NOT export PluginTable in @bunred/bunadmin
+ * Due to the dynamic import of aliases (@plugin)
+ * PluginTable needs to be defined in each project.
+ * @param team
+ * @param group
+ * @param name
+ * @param hideLoading
+ * @constructor
+ */
+export default function PluginTable({
+  team,
+  group,
+  name,
+  hideLoading
+}: PluginTableProps) {
+  const pluginPath = handleSpecialPlugin({ team, group, name })
+
+  const Plugin = dynamic({
+    loader: () => import(`@plugins/${pluginPath}`),
+    loading: () =>
+      hideLoading ? null : <TableSkeleton title={`${name} loading...`} />
+  })
+
+  return <Plugin />
+}
