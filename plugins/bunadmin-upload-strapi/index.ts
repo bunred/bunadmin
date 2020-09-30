d3269f9ad95aeca7a4d6927e10dcf8c4a4eba62d
Chris533 <ganchengs@gmail.com>
2020-09-30 15:24:16 +0800

    Update upload-strapi (export files)

--- plugins/bunadmin-upload-strapi/index.ts
+++ plugins/bunadmin-upload-strapi/index.ts
@@ -2,5 +2,6 @@ import TypeMedia from "./files/types"
 export type MediaType = TypeMedia
 
 export { default as FileUploader } from "./files/components/FileUploader"
-
+export { default as initData } from "./utils/initData"
+export { default as files } from "./files"
 export * from "./utils/types/file"
