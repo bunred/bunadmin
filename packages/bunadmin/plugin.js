ab823ae71f84f4cb55b0121f84b4fdcee08a4426
Chris533 <ganchengs@gmail.com>
2020-10-01 18:24:01 +0800

    Update plugin.js (generate `pluginsData.ts`)

--- packages/bunadmin/plugin.js
+++ packages/bunadmin/plugin.js
@@ -1,21 +1,21 @@
 const path = require("path")
 const fs = require("fs")
+const rimraf = require("rimraf")
 const Log = require("next/dist/build/output/log")
-const {
-  findPluginsPaths,
-  getPluginsData
-} = require("./lib/utils/node/plugin-action")
+const { findPlugins, getPlugins } = require("./lib/utils/node/plugin-action")
 
 /**
- * Prepare bunadmin plugins to `.bunadmin/dynamic` tmp directory,
- * generate the data file (menu, schema) `dynamic/pluginsData.json` to `plugins/`,
- * generate the index file (export from node_modules) `[group]/[name].js` to
- * `dynamic/[plugin]/[group]/[name]`.
- * @param nodeModulesPath {string}
- * @param pluginsDynamicPath {string}
+ * Prepare bunadmin plugins data to `.bunadmin/dynamic/`, `plugins/` directory,
+ * generate the data file (menu, schema) `dynamic/pluginsData.json` to `.bunadmin/`,
+ * generate the index file (export from node_modules) `[name]/index.js` to
+ * `dynamic/[plugin]/[name]/index.js`.
+ * generate the data file (menu, schema) `pluginsData.ts` to `plugins/`,
+ * @param modulesPath {string}
+ * @param dynamicPath {string}
+ * @param pluginsPath {string}
  * @return {boolean}
  */
-module.exports = ({ nodeModulesPath, pluginsDynamicPath }) => {
+module.exports = ({ modulesPath, dynamicPath, pluginsPath }) => {
   /**
    * Prepare plugins START
    */
@@ -29,26 +29,51 @@ module.exports = ({ nodeModulesPath, pluginsDynamicPath }) => {
   /**
    * Recreate directory .bunadmin/dynamic
    */
-  const rimraf = require("rimraf")
-  rimraf.sync(pluginsDynamicPath)
+  rimraf.sync(dynamicPath)
+
   if (!fs.existsSync(".bunadmin")) {
     fs.mkdirSync(".bunadmin")
   }
-  if (!fs.existsSync(pluginsDynamicPath)) {
-    fs.mkdirSync(pluginsDynamicPath)
+  if (!fs.existsSync(dynamicPath)) {
+    fs.mkdirSync(dynamicPath)
   }
 
-  const pluginsPaths = findPluginsPaths(nodeModulesPath)
-  const pluginsData = getPluginsData(pluginsPaths)
+  const pluginsPaths = findPlugins(modulesPath)
+  const pluginsData = getPlugins(pluginsPaths)
+
+  /**
+   * Generate `pluginsData.ts` in `plugins/`
+   */
+  const customPluginsPaths = findPlugins(pluginsPath)
+  let importLine = ""
+  let arrayLine = ""
+  customPluginsPaths.map((path, i) => {
+    const name = path.replace(`${pluginsPath}/`, "")
+    const varName = `data_${i + 1}`
+    importLine += `
+import { initData as ${varName} } from "./${name}"`
+    const spl = i + 1 === customPluginsPaths.length ? "" : ", "
+    arrayLine += `...${varName}${spl}`
+  })
+  const tsContent = `import { IPluginData } from "@bunred/bunadmin"
+${importLine}
+
+export const data: IPluginData[] = [${arrayLine}]
+`
+  fs.writeFile(`${pluginsPath}/pluginsData.ts`, tsContent, e => {
+    if (e) Log.error("cannot generating pluginsData.ts: " + e)
+  })
 
   /**
    * Merge pluginsData
    * Generate pluginsSchema ([plugin]/[group]/[name].js)
    * @type {*[]}
    */
-
   pluginsPaths.map(async pathItem => {
     if (typeof pathItem !== "string") return
+    if (pathItem.indexOf("node_modules") < 0) {
+      return
+    }
 
     let plugin
     try {
@@ -63,7 +88,7 @@ module.exports = ({ nodeModulesPath, pluginsDynamicPath }) => {
     }
 
     /**
-     * Generating plugin files
+     * Generating plugin files under `dynamic/`
      * mkdir directories [plugin], [plugin]/[group]
      * create files [plugin]/[group]/[name].js
      */
@@ -73,7 +98,7 @@ module.exports = ({ nodeModulesPath, pluginsDynamicPath }) => {
       /**
        * Recreate directory dynamic/[plugin]
        */
-      const savePluginPath = path.resolve(pluginsDynamicPath, pluginName)
+      const savePluginPath = path.resolve(dynamicPath, pluginName)
       if (!fs.existsSync(savePluginPath)) {
         await fs.mkdirSync(savePluginPath)
       }
@@ -92,12 +117,12 @@ module.exports = ({ nodeModulesPath, pluginsDynamicPath }) => {
         })
 
         const saveIndexPath = path.resolve(
-          pluginsDynamicPath,
+          dynamicPath,
           `${pluginName}/index.js`
         )
         const saveIndexContent = `export * from "${pluginName}"`
         fs.writeFile(saveIndexPath, saveIndexContent, e => {
-          if (e) Log.error("cannot generating plugin index: " + e)
+          if (e) Log.error("cannot generating plugin index.js: " + e)
         })
       })
     } catch (e) {
@@ -106,7 +131,7 @@ module.exports = ({ nodeModulesPath, pluginsDynamicPath }) => {
   })
 
   const name = "pluginsData.json"
-  const savePath = path.resolve(pluginsDynamicPath, name)
+  const savePath = path.resolve(dynamicPath, name)
   fs.writeFile(savePath, JSON.stringify(pluginsData), "utf8", () => {
     /**
      * Prepare plugins END
