fc03f4e8a2248061327793bf61a23289b1ca09a8
Chris533 <ganchengs@gmail.com>
2020-10-05 23:20:42 +0800

    Update source-graphql (add bulkDelete)

--- packages/bunadmin-source-graphql/index.ts
+++ packages/bunadmin-source-graphql/index.ts
@@ -2,3 +2,4 @@ export * from "./types"
 
 export { default as editableCtrl } from "./controllers/editableCtrl"
 export { default as dataCtrl } from "./controllers/dataCtrl"
+export { default as bunkDeleteCtrl } from "./controllers/bulkDeleteCtrl"
